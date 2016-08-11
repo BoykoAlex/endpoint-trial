46cf3ce92a17a6b41102625649848505a6c63b5e
BoykoAlex <aboyko@pivotal.io>
2016-08-11 15:42:36 -0400

    Another attempt to fix mvc endpoint

--- trial/src/main/java/com/trial/HelloMvcEndpoint.java
+++ trial/src/main/java/com/trial/HelloMvcEndpoint.java
@@ -3,12 +3,21 @@ package com.trial;
 import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.boot.actuate.endpoint.mvc.AbstractEndpointMvcAdapter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ConfigurationProperties(prefix = "endpoints.hello")
 public class HelloMvcEndpoint extends AbstractEndpointMvcAdapter<Endpoint<?>> {
 
 	public HelloMvcEndpoint(Endpoint<?> delegate) {
 		super(delegate);
+	}
+	
+	@ResponseBody
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return (String) getDelegate().invoke();
 	}
 
 }
