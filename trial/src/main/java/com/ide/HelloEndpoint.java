b5f70e9eb0ede01d3b220a14bdbd85f0d4bd8ceb
BoykoAlex <aboyko@pivotal.io>
2016-08-15 14:49:24 -0400

    Remove actuator dependency

--- trial/src/main/java/com/ide/HelloEndpoint.java
+++ trial/src/main/java/com/ide/HelloEndpoint.java
@@ -1,29 +1,35 @@
 package com.ide;
 
-import org.springframework.boot.actuate.endpoint.Endpoint;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Component
-public class HelloEndpoint implements Endpoint<String> {
+@Controller
+public class HelloEndpoint {
 
-	@Override
-	public String getId() {
-		return "hello";
+//	@Override
+//	public String getId() {
+//		return "hello";
+//	}
+//
+//	@Override
+//	public boolean isEnabled() {
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean isSensitive() {
+//		return true;
+//	}
+//
+//	@Override
+//	public String invoke() {
+//		return "Hello Alex! Congrats you've added a new endpoint!";
+//	}
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello Alex!";
 	}
-
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
-
-	@Override
-	public boolean isSensitive() {
-		return true;
-	}
-
-	@Override
-	public String invoke() {
-		return "Hello Alex! Congrats you've added a new endpoint!";
-	}
-
 }
