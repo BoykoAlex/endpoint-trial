53db9d309aee11101b17521b2a414e4bd7232678
Andy Clement <aclement@pivotal.io>
2016-08-16 14:36:19 -0700

    First drop of filewatcher and compiler

---
+++ trial/src/main/java/com/ide/Appy.java
@@ -0,0 +1,12 @@
+package com.ide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appy {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appy.class, args);
+	}
+}
